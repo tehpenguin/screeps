05669d00942dc0f7a3d1a6e965cb2f360e2ae4f6
tehpenguin <jmchx8@gmail.com>
2023-02-14 12:39:39 -0700

    Update role.wall.js

--- role.wall.js
+++ role.wall.js
@@ -7,7 +7,7 @@ var roleWall = {
             // switch state
             creep.memory.repairing = false;
             creep.say("harvest");
-            console.log(creep.memory.repairing, "wall test")
+            //console.log(creep.memory.repairing, "wall test")
         }
         // if creep is harvesting energy but is full
        else if (creep.store.getFreeCapacity(RESOURCE_ENERGY) == 0) {
@@ -15,42 +15,51 @@ var roleWall = {
             creep.memory.repairing = true;
             creep.say("rpr/build");
         }
-console.log(creep.memory.repairing, "wall test2")
+//console.log(creep.memory.repairing, "wall test2")
         // if creep is supposed to repair something
        if (creep.memory.repairing == true) {
-          (creep.room.name !== creep.memory.homeRoom) 
-        creep.moveTo(new RoomPosition(25,25,creep.memory.homeRoom)) ;
-            // find closest structure with less than max hits
-            // Exclude walls because they have way too many max hits and would keep
-            // our repairers busy forever. We have to find a solution for that later.
-            var structure = creep.pos.findClosestByRange(FIND_STRUCTURES, {
-                // the second argument for findClosestByPath is an object which takes
-                // a property called filter which can be a function
-                // we use the arrow operator to define it
-                filter:  (s) => s.hits < s.hitsMax &&  structure == STRUCTURE_WALL 
-             
+       
+
+        var target = undefined;
+
+        // loop with increasing percentages
+        for (let percentage = 0.0001; percentage <= 1; percentage = percentage + 0.0001){
+            // find a wall with less than percentage hits
+
+            // for some reason this doesn't work
+            // target = creep.pos.findClosestByPath(walls, {
+            //     filter: (s) => s.hits / s.hitsMax < percentage
+            // });
+
+            // so we have to use this
+            target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+                filter: (s) => s.structureType == STRUCTURE_WALL &&
+                               s.hits / s.hitsMax < percentage
             });
 
-            // if we find one
-            if (structure != undefined) {
-                // try to repair it, if it is out of range
-                if (creep.repair(structure) == ERR_NOT_IN_RANGE) {
-                    // move towards it
-                    creep.moveTo(structure);
-                }
+            // if there is one
+            if (target != undefined) {
+                // break the loop
+                break;
             }
-            // if we can't fine one
-            /**else {
-                // look for construction sites
-                roleBuilder.run(creep);
-            } **/
+        }
+
+        // if we find a wall that has to be repaired
+        if (target != undefined) {
+            // try to repair it, if not in range
+            if (creep.repair(target) == ERR_NOT_IN_RANGE) {
+                // move towards it
+                console.log(target,"wall target")
+                creep.moveTo(target,{visualizePathStyle: {stroke: '#00FF00'},range:1});
+            }
+        }
         }
         // if creep is supposed to harvest energy from source
         else {
             // find closest source
-            var source = creep.pos.findClosestByRange(FIND_SOURCES_ACTIVE);
+            //var source = creep.pos.findClosestByRange(FIND_SOURCES_ACTIVE);
             // try to harvest energy, if the source is not in range
-            let storage = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+            let storage = creep.pos.findClosestByRange(FIND_STRUCTURES, {
                 // the second argument for findClosestByPath is an object which takes
                 // a property called filter which can be a function
                 // we use the arrow operator to define it
